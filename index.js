e5ad1c7dd5ccb3c0f04760538a895afb826da412
Your Namefotostoys <ciel2000@gmail.com>
2022-08-31 19:06:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const app = require("express")();
+
+let chrome = {};
+let puppeteer;
+
+// vercer run in AWS-LAMBDA SERVERS
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  // if in production
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core"); //light puppeter
+} else {
+  // locally
+  puppeteer = require("puppeteer"); // include chrome
+}
+
+app.get("/", async (req, res) => {
+  let options = {};
+
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
+
+  try {
+    let browser = await puppeteer.launch(options);
+
+    let page = await browser.newPage();
+    // await page.goto("https://www.google.com");
+    await page.goto("http://www.dolar-blue.com/");
+
+    let compraBlue = await page.$eval(
+      ".values > .compra > .val",
+      (el) => el.textContent
+    );
+  
+    let ventaBlue = await page.$eval(
+      ".values > .venta > .val",
+      (el) => el.textContent
+      // (el) => el.innerHTML
+    );
+  
+    let actualizado = await page.$eval(
+      ".update > .container",
+      (el) => el.textContent
+    );
+  
+    actualizado = actualizado.slice(15);
+
+
+    res.send("\nLast Updated: " +
+    actualizado +
+    " - \nBlue Buy: " +
+    compraBlue +
+    " - \nBlue Sell: " +
+    ventaBlue +
+    " \n")
+
+
+    // res.send(await page.title());
+
+    // await browser.close();
+
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
+
+module.exports = app;
